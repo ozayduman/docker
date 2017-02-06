aa2d695505f493de37ae0b6ff793f3e184657eb4
ozayd <ozayd@intra.local>
2017-02-06 10:24:15 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # docker
+## wait-for-it
+A simple shell script to wait for processes to come up
---
+++ wait-for-it/wait-for-it.sh
@@ -0,0 +1,22 @@
+# file run.sh
+#!/bin/sh
+#HOST_PORT
+#HOST_ADDRESS
+#TIME_OUT_SECONDS
+echo "@Author ozay.duman"
+WAIT=0
+while ! nc -z $HOST_ADDRESS $HOST_PORT; do
+  sleep 1
+  WAIT=$(($WAIT + 1))
+  if [ "$WAIT" -gt 15 ]; then
+     echo "Error: Timeout wating for Postgres to start"
+     exit 1
+  fi
+done
+
+if  nc -z $HOST_ADDRESS $HOST_PORT;  then
+	echo ">> Connected!"
+	exec $COMMAND
+else
+	echo "Timeout for host address"
+fi
